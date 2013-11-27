20fe0897c884f802396140eeb8b99a2a2f28581c
Steven Occhipinti <dev@stevenocchipinti.com>
2013-11-27 22:53:53 +1100

    Basic nested form for events with timeslots

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ActiveRecord::Base
+  has_many :timeslots, dependent: :destroy
+  accepts_nested_attributes_for :timeslots
 end
--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -1,2 +1,3 @@
 class Timeslot < ActiveRecord::Base
+  belongs_to :event
 end
