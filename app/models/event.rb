c246a7e1613a1406488caac5ce0e89b2cb00e242
Steve Occhipinti <dev@stevenocchipinti.com>
2013-11-30 01:25:20 +1100

    Reject the timeslots if they are left blank

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ActiveRecord::Base
   has_many :timeslots, dependent: :destroy
-  accepts_nested_attributes_for :timeslots, allow_destroy: true
+
+  accepts_nested_attributes_for :timeslots,
+    allow_destroy: true,
+    reject_if: lambda { |t| t[:datetime_string].blank? }
 end
