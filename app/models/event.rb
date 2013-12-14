fcf25e437d46dbf7e5d2932acacc6a89ca6b56a0
Steven Occhipinti <dev@stevenocchipinti.com>
2013-12-14 18:38:03 +1100

    Start this branch with an empty directory

--- app/models/event.rb
+++
@@ -1,7 +0,0 @@
-class Event < ActiveRecord::Base
-  has_many :timeslots, dependent: :destroy
-
-  accepts_nested_attributes_for :timeslots,
-    allow_destroy: true,
-    reject_if: lambda { |t| t[:datetime_string].blank? }
-end
--- app/views/events/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @event, :name, :description, :created_at, :updated_at
