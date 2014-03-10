56d5f200642baed99f2eaebb35756d242a1e36b9
Steve Occhipinti <dev@stevenocchipinti.com>
2014-03-10 17:52:07 +1100

    WIP: A cuke for voting

--- features/step_definitions/event_steps.rb
+++ features/step_definitions/event_steps.rb
@@ -2,6 +2,27 @@ Given(/^today is "(.*?)"$/) do |datetime|
   Timecop.travel(Chronic.parse(datetime))
 end
 
+Given(/^an? "(.*?)" event with the following timeslots exists:$/) do |name, timeslots|
+  # TODO: Probably should make a factory for this?
+  Event.create(
+    name: name,
+    description: "Some description",
+    timeslots: timeslots.raw.flatten.collect do |timeslot|
+      Timeslot.new.tap {|t| t.datetime_string = timeslot }
+    end
+  )
+end
+
+When(/^I vote for the following timeslots:$/) do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the "(.*?)" event should have the following votes:$/) do |arg1, table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
 When(/^I (fill in|create) an? "(.*?)" event with the following timeslots:$/) do |action, name, timeslots|
   visit new_event_path
   fill_in "Name", with: name
